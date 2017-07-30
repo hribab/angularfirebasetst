c737f964ccf0804895640a7ab16b1518c73f87ca
hribab <itzhari.g@gmail.com>
2017-07-30 13:00:44 -0700

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import * as firebase from 'firebase';
 
 import { AppComponent } from './app.component';
 
